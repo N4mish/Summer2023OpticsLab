306c8c7bb89e2de3e6004b4e128d683f89ecfae6
Namish <namishkukreja@gmail.com>
2023-06-14 13:21:46 -0500

    June 14 updates to statetomography.py

--- statetomography.py
+++ statetomography.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from scipy.linalg import sqrtm
 # constants
 x_matrix = np.matrix([[0, 1],
                       [1, 0]])
@@ -14,11 +14,14 @@ z_matrix = np.matrix([[1, 0],
 def expect(zero, one):
     return (zero - one) / (zero + one)
 
-def find_density_matrix(x_0, x_1, y_0, y_1, z_0, z_1):
+def state_tomo(x_0, x_1, y_0, y_1, z_0, z_1):
     return (1/2) * ( np.identity(2)
                    + (expect(x_0, x_1) * x_matrix)
                    + (expect(y_0, y_1) * y_matrix)
                    + (expect(z_0, z_1) * z_matrix) )
+
+def fidelity(rho, sigma):
+    return np.trace(sqrtm(np.matmul(sqrtm(sigma), np.matmul(rho, sqrtm(sigma)))))
 
 x_0 = float(input("Input the count of 0 for X. "))
 x_1 = float(input("Input the count of 1 for X. "))
@@ -27,5 +30,32 @@ y_1 = float(input("Input the count of 1 for Y. "))
 z_0 = float(input("Input the count of 0 for Z. "))
 z_1 = float(input("Input the count of 1 for Z. "))
 
-res = find_density_matrix(x_0, x_1, y_0, y_1, z_0, z_1)
-print(res)
+denMat1 = state_tomo(x_0, x_1, y_0, y_1, z_0, z_1)
+
+
+response = input("Type Y if you would like to enter information for a second matrix.").lower()
+
+if (response == 'y'):
+    x_01 = float(input("Input the count of 0 for X. "))
+    x_11 = float(input("Input the count of 1 for X. "))
+    y_01 = float(input("Input the count of 0 for Y. "))
+    y_11 = float(input("Input the count of 1 for Y. "))
+    z_01 = float(input("Input the count of 0 for Z. "))
+    z_11 = float(input("Input the count of 1 for Z. "))
+
+    denMat2 = state_tomo(x_01, x_11, y_01, y_11, z_01, z_11)
+
+    print(f"matrix 1: \n{denMat1}\n matrix 2: \n{denMat2}")
+
+    response = input("Type Y if you would like to measure fidelity.").lower()
+    if (response == 'y'):
+        print(f"fidelity: {np.real(fidelity(denMat1, denMat2))}")
+
+else:
+    print(f"matrix: \n{denMat1}")
+
+
+
+
+
+
